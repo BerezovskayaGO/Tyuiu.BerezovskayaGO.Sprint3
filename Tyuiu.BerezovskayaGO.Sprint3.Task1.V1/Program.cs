85fec2ea9d9bdaebb8537791d7586c64fd13b13b
Tyuiu.BerezovskayaGO@outlook.com <Tyuiu.BerezovskayaGO@outlook.com>
2024-10-22 22:15:12 +0500

    Task1

---
+++ Tyuiu.BerezovskayaGO.Sprint3.Task1.V1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.BerezovskayaGO.Sprint3.Task1.V1.Lib;
+namespace Tyuiu.BerezovskayaGO.Sprint3.Task1.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Березовская Г. О. | АСОиУБ - 24-1";
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                                *");
+            Console.WriteLine("* Тема: Оператор цикла while                                                                *");
+            Console.WriteLine("* Задание #1                                                                               *");
+            Console.WriteLine("* Вариант #1                                                                               *");
+            Console.WriteLine("* Выполнила: Березовская Галина Олеговна | АСОиУБ - 24-1                                   *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                 *");
+            Console.WriteLine("* Написать программу используя цикл while,                                                 *");
+            Console.WriteLine("* которая вычисляет произведение ряда по формуле.                                          *");
+            Console.WriteLine("*                                                                                          *");
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("********************************************************************************************");
+
+            int startValue = 1;
+            int stopValue = 7;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                               *");
+            Console.WriteLine("********************************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
