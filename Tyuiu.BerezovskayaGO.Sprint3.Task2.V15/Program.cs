eb7451c752b6084394674ae67746ddbdcdc13afc
Tyuiu.BerezovskayaGO@outlook.com <Tyuiu.BerezovskayaGO@outlook.com>
2024-10-26 17:58:53 +0500

    Task2

---
+++ Tyuiu.BerezovskayaGO.Sprint3.Task2.V15/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.BerezovskayaGO.Sprint3.Task2.V15.Lib;
+namespace Tyuiu.BerezovskayaGO.Sprint3.Task2.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Березовская Г. О. | АСОиУБ - 24-1";
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                                *");
+            Console.WriteLine("* Тема: Оператор цикла do-while                                                            *");
+            Console.WriteLine("* Задание #2                                                                               *");
+            Console.WriteLine("* Вариант #15                                                                              *");
+            Console.WriteLine("* Выполнила: Березовская Галина Олеговна | АСОиУБ - 24-1                                   *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                 *");
+            Console.WriteLine("* Написать программу используя цикл do...while,                                            *");
+            Console.WriteLine("* которая вычисляет сумму ряда по формуле, при a=0,5.                                      *");
+            Console.WriteLine("*                                                                                          *");
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("********************************************************************************************");
+
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 20;
+
+            Console.WriteLine("Переменная a = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                               *");
+            Console.WriteLine("********************************************************************************************");
+
+            Console.WriteLine("Сумму ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
