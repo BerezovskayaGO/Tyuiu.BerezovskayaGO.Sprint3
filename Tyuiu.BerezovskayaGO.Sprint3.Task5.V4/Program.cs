ac18665164e6584b897aec85d06a9d300b6a7302
Tyuiu.BerezovskayaGO@outlook.com <Tyuiu.BerezovskayaGO@outlook.com>
2024-10-28 21:01:43 +0500

    Task5

---
+++ Tyuiu.BerezovskayaGO.Sprint3.Task5.V4/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.BerezovskayaGO.Sprint3.Task5.V4.Lib;
+namespace Tyuiu.BerezovskayaGO.Sprint3.Task5.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Березовская Г. О. | АСОиУБ - 24-1";
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                                *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                                    *");
+            Console.WriteLine("* Задание #5                                                                               *");
+            Console.WriteLine("* Вариант #4                                                                               *");
+            Console.WriteLine("* Выполнила: Березовская Галина Олеговна | АСОиУБ - 24-1                                   *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                 *");
+            Console.WriteLine("* Написать программу, которая вычисляет сумму ряда по формуле, при X=5.                    *");
+            Console.WriteLine("*                                                                                          *");
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("********************************************************************************************");
+
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 13;
+
+            Console.WriteLine("Переменная x = ");
+            Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+            Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
+            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue2);
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                               *");
+            Console.WriteLine("********************************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+
+            Console.ReadKey();
+        }
+    }
+}
