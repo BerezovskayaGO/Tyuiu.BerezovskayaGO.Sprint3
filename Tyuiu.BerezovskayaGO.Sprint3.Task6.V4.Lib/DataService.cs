2c436440fe38ddda9c83c9404b7b4f4043956a7c
Tyuiu.BerezovskayaGO@outlook.com <Tyuiu.BerezovskayaGO@outlook.com>
2024-10-30 19:03:36 +0500

    Task6

---
+++ Tyuiu.BerezovskayaGO.Sprint3.Task6.V4.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.BerezovskayaGO.Sprint3.Task6.V4.Lib
+{
+    public class DataService : ISprint3Task6V4
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int x;
+            int count = 0;
+
+            for (x = startValue; x <= stopValue; x++)
+            {
+                for (int d = 1; d <= x; d++)
+                {
+                    if (x % d == 0)
+                    {
+                        if (d > 9)
+                        {
+                            count++;
+                        }
+                    }
+                }     
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.BerezovskayaGO.Sprint3.Task6.V4/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.BerezovskayaGO.Sprint3.Task6.V4.Lib;
+namespace Tyuiu.BerezovskayaGO.Sprint3.Task6.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Березовская Г. О. | АСОиУБ - 24-1";
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                                *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                                    *");
+            Console.WriteLine("* Задание #6                                                                               *");
+            Console.WriteLine("* Вариант #4                                                                               *");
+            Console.WriteLine("* Выполнила: Березовская Галина Олеговна | АСОиУБ - 24-1                                   *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                 *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих числовому отрезку      *");
+            Console.WriteLine("* [14, 20] количество всех делителей больше 9                                              *");
+            Console.WriteLine("*                                                                                          *");
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("********************************************************************************************");
+
+            int startValue = 14;
+            int stopValue = 20;
+
+            Console.WriteLine("Начало отрезка = " + startValue);
+            Console.WriteLine("Конец отрезка = " + stopValue);
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                               *");
+            Console.WriteLine("********************************************************************************************");
+
+            Console.WriteLine("Количество делителей = " + ds.GetSumTheDivisors(startValue, startValue));
+
+            Console.ReadKey();
+        }
+    }
+}
